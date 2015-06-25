2492dcf694daa71bc552cd124a73246550739cf3
Megan Dunbar <meganleedunbar@gmail.com>
2015-06-25 17:16:45 -0400

    position logging via component and route

--- app/components/weather-display.js
+++ app/components/weather-display.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model: function(){
+    // return new Ember.RSVP.Promise(function(resolve, reject) {
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+// });
+});
+}
 });
--- app/router.js
+++ app/router.js
@@ -2,10 +2,18 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
-});
+  model: function() {
+    return Ember.$.ajax("https://api.forecast.io/forecast/01990fee22f745663bd4d38f6cc2d36c/37.8267,-122.423", {dataType: 'jsonp'});
+  }
+  });
 
 Router.map(function() {
 });
+
+// Router.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return Ember.$.ajax("https://api.forecast.io/forecast/01990fee22f745663bd4d38f6cc2d36c/37.8267,-122.423", {dataType: 'jsonp'});
+//   }
+// });
 
 export default Router;
